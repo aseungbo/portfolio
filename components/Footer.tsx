c7160d8e5ebf1f4e2efb94852ca654be379e3f05
seungb5 <seungbo1112@gmail.com>
2023-03-03 19:12:23 +0900

    feat: Apply tailwindcss

---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer(): JSX.Element {
+  return <footer>This is footer</footer>;
+}
---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header(): JSX.Element {
+  return (
+    <header className="flex justify-between px-4 w-full">
+      <button className=" ">Home</button>
+      <div></div>
+    </header>
+  );
+}
